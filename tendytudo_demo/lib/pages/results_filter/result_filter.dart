f6b573d6cadfe33f7ab0f101b853f6a2378b7ee7
LuisFernando123 <l.fernandodasilva@gmail.com>
2020-10-22 19:51:47 -0300

    ajustado rota

---
+++ tendytudo_demo/lib/pages/results_filter/result_filter.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ResultFilterPage extends StatefulWidget {
+  @override
+  _ResultFilterPageState createState() => _ResultFilterPageState();
+}
+
+class _ResultFilterPageState extends State<ResultFilterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
