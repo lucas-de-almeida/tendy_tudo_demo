16c356d33de3bdfeef5869c956fead714b8ed08d
LuisFernando123 <l.fernandodasilva@gmail.com>
2020-10-21 21:55:25 -0300

    add as rotas na main

--- tendytudo_demo/lib/main.dart
+++ tendytudo_demo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:tendytudo_demo/pages/filters/filter_page.dart';
 import 'package:tendytudo_demo/pages/home/home_page.dart';
 import 'package:tendytudo_demo/pages/profile/profile.dart';
@@ -10,9 +11,23 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FiltersPage(),
+      initialRoute: '/',
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => HomePage(),
+        ),
+        GetPage(
+          name: '/filter',
+          page: () => FiltersPage(),
+        ),
+        GetPage(
+          name: '/profile',
+          page: () => ProfilePage(),
+        ),
+      ],
     );
   }
 }
