5dfbdcab5dee51cfc07968f4291a11475f029951
LuisFernando123 <l.fernandodasilva@gmail.com>
2020-10-21 21:51:17 -0300

    criado layout tela de busca

--- tendytudo_demo/lib/main.dart
+++ tendytudo_demo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tendytudo_demo/pages/filters/filter_page.dart';
 import 'package:tendytudo_demo/pages/home/home_page.dart';
 import 'package:tendytudo_demo/pages/profile/profile.dart';
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Profile(),
+      home: FiltersPage(),
     );
   }
 }
---
+++ tendytudo_demo/lib/pages/filters/filter_page.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getwidget/getwidget.dart';
+
+class FiltersPage extends StatefulWidget {
+  @override
+  _FiltersPageState createState() => _FiltersPageState();
+}
+
+class _FiltersPageState extends State<FiltersPage> {
+  double _value = 0.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Procurar...'),
+        backgroundColor: Color(0xFF007838),
+        automaticallyImplyLeading: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: Get.back,
+          )
+        ],
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GFAccordion(
+                    title: 'Ordenação padrao',
+                    textStyle: TextStyle(color: Colors.white, fontSize: 16),
+                    collapsedTitleBackgroundColor: Color(0xFF007838),
+                    expandedTitleBackgroundColor: Color(0xFF007838),
+                    contentBackgroundColor: Colors.blueAccent,
+                    contentChild: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Checkbox(value: false, onChanged: null),
+                            Text('Ordenar pelo maior valor'),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Checkbox(value: false, onChanged: null),
+                            Text('Ordenar pelo maior valor'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  GFAccordion(
+                    title: 'Preços',
+                    textStyle: TextStyle(color: Colors.white, fontSize: 16),
+                    collapsedTitleBackgroundColor: Color(0xFF007838),
+                    expandedTitleBackgroundColor: Color(0xFF007838),
+                    contentBackgroundColor: Colors.blueAccent,
+                    contentChild: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Checkbox(value: false, onChanged: null),
+                            Text('Ordenar pelo maior valor'),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Checkbox(value: false, onChanged: null),
+                            Text('Ordenar pelo menor valor'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  GFAccordion(
+                    title: 'Avaliação',
+                    textStyle: TextStyle(color: Colors.white, fontSize: 16),
+                    collapsedTitleBackgroundColor: Color(0xFF007838),
+                    expandedTitleBackgroundColor: Color(0xFF007838),
+                    contentBackgroundColor: Colors.blueAccent,
+                    contentChild: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Checkbox(value: false, onChanged: null),
+                            Text('Ordenar pelo melhor avaliação'),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Checkbox(value: false, onChanged: null),
+                            Text('Ordenar pelo menor avaliação'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  GFAccordion(
+                    title: 'Tempo de entrega',
+                    textStyle: TextStyle(color: Colors.white, fontSize: 16),
+                    collapsedTitleBackgroundColor: Color(0xFF007838),
+                    expandedTitleBackgroundColor: Color(0xFF007838),
+                    contentBackgroundColor: Colors.blueAccent,
+                    contentChild: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Checkbox(value: false, onChanged: null),
+                            Text('Ordenar pelo maior Tempo de entrega'),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Checkbox(value: false, onChanged: null),
+                            Text('Ordenar pelo menor Tempo de entrega'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  GFAccordion(
+                    title: 'Taxa de entrega',
+                    textStyle: TextStyle(color: Colors.white, fontSize: 16),
+                    collapsedTitleBackgroundColor: Color(0xFF007838),
+                    expandedTitleBackgroundColor: Color(0xFF007838),
+                    contentBackgroundColor: Colors.blueAccent,
+                    contentChild: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Checkbox(value: false, onChanged: null),
+                            Text('Ordenar pelo maior taxa de entrega'),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Checkbox(value: false, onChanged: null),
+                            Text('Ordenar pelo menor taxa de entrega'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  GFAccordion(
+                    title: 'Distancia',
+                    textStyle: TextStyle(color: Colors.white, fontSize: 16),
+                    collapsedTitleBackgroundColor: Color(0xFF007838),
+                    expandedTitleBackgroundColor: Color(0xFF007838),
+                    contentBackgroundColor: Colors.blueAccent,
+                    contentChild: Column(
+                      children: [
+                        Text(
+                          'Distancia maxima de 30 km',
+                        ),
+                        Row(
+                          children: [
+                            Text('0'),
+                            Expanded(
+                              flex: 30,
+                              child: Slider(
+                                value: _value,
+                                activeColor: Colors.black,
+                                inactiveColor: Colors.black,
+                                min: 0.0,
+                                max: 30.0,
+                                divisions: 100,
+                                label: '${_value.round()}',
+                                onChanged: (value) {
+                                  setState(() {
+                                    _value = value;
+                                  });
+                                },
+                              ),
+                            ),
+                            Text('30'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GFButton(
+                  onPressed: () {},
+                  text: "Procurar",
+                  color: Color(0xFF007838),
+                  size: GFSize.MEDIUM,
+                  fullWidthButton: true,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
